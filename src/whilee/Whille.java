89536d68a7da1867997dddc918ef807784aa87db
Kelly Lima <kellylimapereira@yahoo.com>
2022-02-28 21:15:07 -0300

    atualização

---
+++ src/whilee/Whille.java
@@ -0,0 +1,23 @@
+package whilee;
+
+import java.util.Scanner;
+
+public class Whille {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		int soma = 0;
+		
+		while (x != 0) {
+			soma = soma + x;
+			x = sc.nextInt();
+		}
+		
+		System.out.println("A soma dos números é : " + soma);
+
+		sc.close();
+	}
+
+}
